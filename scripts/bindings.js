7e01507a03dd89674465e520492493f504ce9bfb
dvdrtrgn <dvdrtrgn@gmail.com>
2015-08-20 15:01:03 -0500

    create bindings scope bubble

--- scripts/bindings.js
+++ scripts/bindings.js
@@ -1,40 +1,56 @@
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 /* global Grav, Canvas */
 
-var TRANSLATION_STEP = 50;
+(function Bind() {
+    var TRANSLATION_STEP = 50;
 
-$('.btnEarthMass').click(Grav.setEarthMode);
-$('.btnMoonMass').click(Grav.setMoonMode);
-$('.btnJupiterMass').click(Grav.setJupiterMode);
+    function up() {
+        Canvas.translate(0, TRANSLATION_STEP);
+    }
+    function left() {
+        Canvas.translate(TRANSLATION_STEP, 0);
+    }
+    function right() {
+        Canvas.translate(-(TRANSLATION_STEP), 0);
+    }
+    function down() {
+        Canvas.translate(0, -(TRANSLATION_STEP));
+    }
 
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+    $('.btnEarthMass').click(Grav.setEarthMode);
+    $('.btnMoonMass').click(Grav.setMoonMode);
+    $('.btnJupiterMass').click(Grav.setJupiterMode);
 
-$('.btnUP').click(function () {
-    Canvas.translate(0, TRANSLATION_STEP);
-});
-$('.btnLEFT').click(function () {
-    Canvas.translate(TRANSLATION_STEP, 0);
-});
-$('.btnRIGHT').click(function () {
-    Canvas.translate(-(TRANSLATION_STEP), 0);
-});
-$('.btnDOWN').click(function () {
-    Canvas.translate(0, -(TRANSLATION_STEP));
-});
+    $('.btnUP').click(up);
+    $('.btnLEFT').click(left);
+    $('.btnRIGHT').click(right);
+    $('.btnDOWN').click(down);
 
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+    $('body').keydown(function (evt) {
+        switch (evt.keyCode) {
+            case 38:
+                return up();
+            case 37:
+                return left();
+            case 39:
+                return right();
+            case 40:
+                return down();
+        }
+    });
 
-$('#startButton').click(Grav.start);
-$('#stopButton').click(Grav.stop);
-$('#resetButton').click(Grav.reset);
-$('#traceButton').click(Grav.toggleTraces);
-$('#clearTraces').click(Grav.clearTraces);
-$('#save').click(Grav.saveSpaceBodies);
-$('#restore').click(Grav.restoreFromOutputArea);
+    $('#startButton').click(Grav.start);
+    $('#stopButton').click(Grav.stop);
+    $('#resetButton').click(Grav.reset);
+    $('#traceButton').click(Grav.toggleTraces);
+    $('#clearTraces').click(Grav.clearTraces);
+    $('#save').click(Grav.saveSpaceBodies);
+    $('#restore').click(Grav.restoreFromOutputArea);
 
-Canvas.getSvgCanvas().onmousedown = function (e) {
-    Grav.onSvgMouseDown(new MultiBrowserMouseEvent(e));
-};
+    Canvas.getSvgCanvas().onmousedown = function (e) {
+        Grav.onSvgMouseDown(new MultiBrowserMouseEvent(e));
+    };
 
-console.log('bindings loaded');
+    console.log('bindings loaded');
+}());
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
